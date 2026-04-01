795c6e7af2b365808f3773ac66b82193cb5e9d3f
yaren1999 <demirciyarenmisra@gmail.com>
2026-04-01 14:57:10 +0300

    feat: Marketplace iade (refund) fonksiyonu ve IToken arayüz güncellemesi eklendi

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 interface IToken {
+    function transfer(address to, uint256 value) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
     function balanceOf(address account) external view returns (uint256) ;
 }
@@ -21,5 +22,14 @@ contract Marketplace {
         require(success, "Transfer basarisiz");
 
         myInventory[msg.sender] += 1;
+    }
+
+    function refundItem() public {
+
+        require(myInventory[msg.sender] > 0, "Iade edilecek urununuz yok");
+        require(token.balanceOf(address(this)) >= itemPrice, "Dukkan kasasinda yeterli token yok");
+        myInventory[msg.sender] -= 1;
+        bool success = token.transfer(msg.sender, itemPrice);
+        require(success, "Iade transferi basarisiz");
     }
 }
--- test/Marketplace.js
+++ test/Marketplace.js
@@ -17,11 +17,20 @@ describe("Marketplace: Satın Alma Testi", function () {
     });
 
     it("Müşteri ürün satın alabilmeli", async function () {
-       
         await token.connect(addr1).approve(await marketplace.getAddress(), 50);
         await marketplace.connect(addr1).buyItem();
 
         expect(await marketplace.myInventory(addr1.address)).to.equal(1);
         expect(await token.balanceOf(addr1.address)).to.equal(50);
+    });
+
+    it("Müşteri ürünü iade edebilmeli ve parasını geri alabilmeli", async function () {
+        const marketplaceAddress = await marketplace.getAddress();
+        await token.connect(addr1).approve(marketplaceAddress, 50);
+        await marketplace.connect(addr1).buyItem();
+        await marketplace.connect(addr1).refundItem();
+
+        expect(await token.balanceOf(addr1.address)).to.equal(100); 
+        expect(await marketplace.myInventory(addr1.address)).to.equal(0);
     });
 });
