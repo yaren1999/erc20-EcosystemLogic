3b07ceee06873672cb44e6ad463854056337d06f
yaren1999 <demirciyarenmisra@gmail.com>
2026-04-01 17:33:20 +0300

    feat: Staking kontratı oluşturuldu ve ilk para kilitleme (stake) testi başarıyla tamamlandı

---
+++ contracts/Staking.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IToken {
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract Staking {
+    IToken public token;
+    
+  
+    mapping(address => uint256) public stakedBalance;
+  
+    mapping(address => uint256) public stakingTimestamp;
+
+    constructor(address _tokenAddress) {
+        token = IToken(_tokenAddress);
+    }
+
+ 
+    function stake(uint256 amount) public {
+        require(amount > 0, "Sifir miktar yatirilamaz");
+        bool success = token.transferFrom(msg.sender, address(this), amount);
+        require(success, "Stake islemi basarisiz");
+
+        stakedBalance[msg.sender] += amount;
+        stakingTimestamp[msg.sender] = block.timestamp; 
+    }
+}
---
+++ test/Staking.js
@@ -0,0 +1,25 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Staking: Para Kilitleme", function () {
+    let token, staking, owner, addr1;
+
+    beforeEach(async function () {
+        [owner, addr1] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("Token");
+        token = await Token.deploy(1000);
+
+        const Staking = await ethers.getContractFactory("Staking");
+        staking = await Staking.deploy(await token.getAddress());
+        await token.transfer(addr1.address, 200);
+    });
+
+    it("Kullanıcı token kilitleyebilmeli (Stake)", async function () {
+        const stakingAddress = await staking.getAddress();
+        await token.connect(addr1).approve(stakingAddress, 100);
+        await staking.connect(addr1).stake(100);
+       
+        expect(await staking.stakedBalance(addr1.address)).to.equal(100);
+        expect(await token.balanceOf(addr1.address)).to.equal(100);
+    });
+});
